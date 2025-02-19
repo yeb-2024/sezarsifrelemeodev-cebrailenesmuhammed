bdad296966e8f7f9c88f4a47704c7d38abb1e6c9
cebrail <cemg346807@gmail.com>
2025-02-19 12:47:22 +0300

    Main ve frame eklendi

--- untitled/src/main/java/org/example/CaesarŞifreleme.java
+++ untitled/src/main/java/org/example/CaesarŞifreleme.java
@@ -1,4 +1,17 @@
 ﻿package org.example;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class CaesarŞifreleme {
+    JFrame frame;
+
+    CaesarŞifreleme(){
+        frame = new JFrame("Sezer şifrelem");
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(4,1));
+
+
+    }
 }
